1ece70cc43cd542a29d6644f553aa6c0b07c8a2d
Pauline <p.mutuku@alustudent.com>
2024-11-29 04:05:14 +0300

    Updates

---
+++ config/passport.js
@@ -0,0 +1,37 @@
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const User = require('../models/user');
+
+passport.use(new LocalStrategy(
+  async (username, password, done) => {
+    try {
+      const user = await User.findOne({ username });
+      if (!user) {
+        return done(null, false, { message: 'Invalid username or password' });
+      }
+      const isValidPassword = await user.comparePassword(password);
+      if (!isValidPassword) {
+        return done(null, false, { message: 'Invalid username or password' });
+      }
+      return done(null, user);
+    } catch (err) {
+      return done(err);
+    }
+  }
+));
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser(async (id, done) => {
+  try {
+    const user = await User.findById(id);
+    done(null, user);
+  } catch (err) {
+    console.error('Error deserializing user:', err);
+    done(err);
+  }
+});
+
+module.exports = passport;
