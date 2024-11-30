855a078c9f7549266ad4a4f9c1315bcdde2ba717
Pauline <p.mutuku@alustudent.com>
2024-11-30 08:00:57 +0300

    Updates

--- config/db.js
+++ config/db.js
@@ -4,7 +4,7 @@ const mysql = require('mysql2');
 const pool = mysql.createPool({
   host: 'localhost',
   user: 'root',
-  password: 'ericalivia@2004',
+  password: 'Starlin3.',
   database: 'multilingual_file_manager_app',
   waitForConnections: true,
   connectionLimit: 10,
